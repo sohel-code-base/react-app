b4c7ab9eb247c2d8abdf1177742c1a74772a34d4
soheldeveloper <sohelrananayanpur@gmail.com>
2022-08-16 23:10:22 +0600

    update component name

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import "bootstrap/dist/css/bootstrap.min.css"
 // import './index.css';
 // import App from './App';
-import Header from "./components/Navbar";
+import Header from "./components/Header";
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
